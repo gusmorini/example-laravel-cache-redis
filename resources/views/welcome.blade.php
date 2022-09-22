6d3b4c21f6ca29094d59c538ec95bc90be31617a
Gustavo Morini <gustavommh@gmail.com>
2022-09-22 15:24:24 -0300

    layout grid responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,26 +22,42 @@
         }
 
         .container {
-            width: 700px;
-            margin: 0 auto;
+            width: 900px;
+            max-width: 90%;
+            margin: 1rem auto;
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-gap: 2rem;
+        }
+
+        @media (min-width: 576px) {
+            .container {
+                grid-template-columns: 1fr 1fr;
+            }
+        }
+
+        @media (min-width: 992px) {
+            .container {
+                grid-template-columns: 1fr 1fr 1fr;
+            }
         }
 
         .new {
-            margin-bottom: 0.5rem;
-            padding-bottom: 0.5rem;
-            border-bottom: 1px dotted rgb(235, 235, 235);
+            box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+            background-color: #fbfbfb;
         }
 
         .new h1 {
-            font-size: 1.4rem;
+            font-size: 1.2rem;
             background-color: #8faeb7;
             color: white;
-            padding: 0.2rem 0.6rem;
+            padding: 1rem 2rem;
+            border-top-right-radius: 0.5rem;
+            border-top-left-radius: 0.5rem;
         }
 
         .new p {
-            background-color: #fbfbfb;
-            padding: 0.6rem;
+            padding: 2rem;
         }
     </style>
 
