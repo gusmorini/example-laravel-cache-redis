224c9de73c411e7691423ef85c7fd42240d2913c
Gustavo Morini <gustavommh@gmail.com>
2022-09-22 15:46:22 -0300

    compile app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,60 +7,7 @@
     <title>example cache redis</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
-
-    <style>
-        * {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-            font-family: 'Nunito';
-        }
-
-        body {
-            background-color: #f3f1ef;
-            color: #343a3a;
-        }
-
-        .container {
-            width: 900px;
-            max-width: 90%;
-            margin: 1rem auto;
-            display: grid;
-            grid-template-columns: 1fr;
-            grid-gap: 2rem;
-        }
-
-        @media (min-width: 576px) {
-            .container {
-                grid-template-columns: 1fr 1fr;
-            }
-        }
-
-        @media (min-width: 992px) {
-            .container {
-                grid-template-columns: 1fr 1fr 1fr;
-            }
-        }
-
-        .new {
-            box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
-            background-color: #fbfbfb;
-        }
-
-        .new h1 {
-            font-size: 1.2rem;
-            background-color: #8faeb7;
-            color: white;
-            padding: 1rem 2rem;
-            border-top-right-radius: 0.5rem;
-            border-top-left-radius: 0.5rem;
-        }
-
-        .new p {
-            padding: 2rem;
-        }
-    </style>
-
+    <link rel="stylesheet" href="css/app.css">
 </head>
 
 <body>
