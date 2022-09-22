a5534fb0bd45aea1ec0abcdb107e4d5e1190f944
Gustavo Morini <gustavommh@gmail.com>
2022-09-22 14:42:09 -0300

    route resource /

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::Resource('/', App\Http\Controllers\NewsController::class);
